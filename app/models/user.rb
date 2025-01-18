0481fddf751e41942aa4a5469bdb95bb93d05a7f
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2025-01-18 13:22:24 -0500

    added user table and added validations for attrtibutes. added enum for role

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  enum role: { student: 0, tutor: 1}
+
+  validates :user_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+end
